4d657b525c741a0af4b8ec0dca8b7c7afc989735
Théo BL <theo.biasutto-lervat@inria.fr>
2023-07-17 11:51:53 +0200

    :recycle: Create a python package (+ pyproject.toml)

---
+++ megabot/speaker.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+LOVE_QUOTES = os.getenv("LOVE_QUOTES").split(",")
+
+loveQuotes = [
+    LOVE_QUOTES,
+    "Megabot vous aime aussi !",
+]
+
+sortileges = [
+    "☄️ Ich narova glaturto spalovar chapopu ☄️",
+    "❄️ Chlagagla ! ❄️",
+    "🔥 Clatori chagoplapipouf chimana 🔥",
+    "🌪 Iz no qutchapoilu ratofu fourapin cataglae glopoïr 🌪",
+    "⚡️ Calaminochu carl zafo furmanich ip glore stou kopch troa fra koulamifaq ⚡️",
+    "💥 Chakeplikoum ! 💥",
+]
